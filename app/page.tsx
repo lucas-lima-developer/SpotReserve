42b49c5597d433356547b85de8d72b0dbf41f073
Lucas Lima <lucasvinilima15@gmail.com>
2024-11-27 16:28:17 -0300

    feat: mudança visual no input de busca

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <header className="flex flex-col md:flex-row items-center justify-between px-0 lg:px-14 py-6 mb-20">
+      <header className="flex flex-col md:flex-row items-center justify-between px-5 py-6 mb-20">
         <div className="flex items-center gap-2">
           <span className="text-3xl md:mb-0 mb-6 font-bold text-gray-800">Spot Reserve</span>
         </div>
@@ -14,8 +14,8 @@ export default function Home() {
       </header>
       <main>
         <div className="flex items-center justify-center">
-          <form className="flex items-center gap-2 p-4 rounded-full shadow-lg border border-gray-300">
-            <div className="flex flex-col">
+          <form className="flex flex-col gap-4 lg:flex-row lg:items-center lg:gap-6 p-6 rounded-2xl mb-10">
+            <div className="flex flex-col flex-1 min-w-[180px]">
               <label htmlFor="onde" className="text-xs text-gray-600 font-medium hidden">
                 Onde
               </label>
@@ -23,21 +23,20 @@ export default function Home() {
                 type="text"
                 id="onde"
                 placeholder="Onde"
-                className="w-32 lg:w-48 px-4 py-2 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full px-6 py-3 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col flex-1 min-w-[180px]">
               <label htmlFor="data" className="text-xs text-gray-600 font-medium hidden">
                 Data
               </label>
               <input
                 type="date"
                 id="data"
-                className="w-32 lg:w-48 px-4 py-2 text-sm border border-gray-300 rounded-md placeholder-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full px-6 py-3 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
-            
-            <div className="flex flex-col">
+            <div className="flex flex-col flex-1 min-w-[180px]">
               <label htmlFor="quantidade" className="text-xs text-gray-600 font-medium hidden">
                 Quantidade de Pessoas
               </label>
@@ -46,12 +45,12 @@ export default function Home() {
                 id="quantidade"
                 placeholder="Qtd Pessoas"
                 min="1"
-                className="w-38 px-4 py-2 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full px-6 py-3 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
             <button
               type="submit"
-              className="bg-blue-500 text-white text-sm font-medium px-6 py-2 rounded-full hover:bg-blue-600 transition"
+              className="bg-blue-500 text-white text-sm font-medium px-8 py-3 rounded-full hover:bg-blue-600 transition flex-shrink-0 w-full lg:w-auto"
             >
               Buscar
             </button>
